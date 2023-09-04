ed6730c65a79c6dd9a527b6f353c067da67337ff
KARE-RAJA-SHEKAR <143408922+KARE-RAJA-SHEKAR@users.noreply.github.com>
2023-09-04 17:19:59 +0530

    Delete CheckPalindrom.java

--- CheckPalindrom.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class CheckPalindrom{
-	public static void main(String args[]){
-		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter a positive Integer to check palindrom or not :");
-	}
-}
