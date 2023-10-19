7e61c436d3c934c6c685d647637d19c811da9e23
Raja shekar kare <143408922+KARE-RAJA-SHEKAR@users.noreply.github.com>
2023-10-19 15:25:18 +0530

    Create JavaMathDemo .java

---
+++ JavaMathDemo .java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class JavaMathDemo {
+    public static void main(String args[]) {
+        double x = 48;
+        double y = 4;
+        System.out.println("Maximum of x and y is " +Math.max(x,y));
+        System.out.println("Minimum of x and y is " +Math.min(x,y));
+        System.out.println("power of x to y is " +Math.pow(x,y));
+        System.out.println("Log10(x) is " +Math.log10(x));
+        System.out.println("Round value of 4.56 is "+Math.round(4.56));
+    }
+}
